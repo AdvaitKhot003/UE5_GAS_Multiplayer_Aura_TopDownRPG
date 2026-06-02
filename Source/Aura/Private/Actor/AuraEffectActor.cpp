3470f4e9b5110ed820cebb3ae63e48072cb44aa7
AdvaitKhot003 <79973168+AdvaitKhot003@users.noreply.github.com>
2026-06-02 21:00:24 +0530

    Instant and Duration Application Policy

--- Source/Aura/Private/Actor/AuraEffectActor.cpp
+++ Source/Aura/Private/Actor/AuraEffectActor.cpp
@@ -18,26 +18,46 @@ void AAuraEffectActor::BeginPlay()
 
 void AAuraEffectActor::ApplyEffectToTargetActor(AActor* TargetActor, const TSubclassOf<UGameplayEffect> GameplayEffectClass)
 {
+	if (!HasAuthority()) return;
+	
 	UAbilitySystemComponent* TargetAsc = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(TargetActor);
 	if (!TargetAsc) return;
 	
-	FGameplayEffectContextHandle EffectContext = TargetAsc->MakeEffectContext();
-	EffectContext.AddSourceObject(this);
+	FGameplayEffectContextHandle EffectContextHandle = TargetAsc->MakeEffectContext();
+	EffectContextHandle.AddSourceObject(this);
 	
 	check(GameplayEffectClass);
 	
-	const FGameplayEffectSpecHandle EffectSpec = TargetAsc->MakeOutgoingSpec(GameplayEffectClass, 1.f, EffectContext);
-	if (!EffectSpec.IsValid()) return;
+	const FGameplayEffectSpecHandle EffectSpecHandle =
+		TargetAsc->MakeOutgoingSpec(GameplayEffectClass, 1.f, EffectContextHandle);
 	
-	TargetAsc->ApplyGameplayEffectSpecToSelf(*EffectSpec.Data.Get());
+	if (!EffectSpecHandle.IsValid()) return;
+	
+	TargetAsc->ApplyGameplayEffectSpecToSelf(*EffectSpecHandle.Data.Get());
 }
 
 void AAuraEffectActor::OnBeginOverlap(AActor* TargetActor)
 {
+	if (InstantApplicationPolicy == EEffectApplicationPolicy::ApplyOnOverlap)
+	{
+		ApplyEffectToTargetActor(TargetActor, InstantGameplayEffectClass);
+	}
 	
+	if (DurationApplicationPolicy == EEffectApplicationPolicy::ApplyOnOverlap)
+	{
+		ApplyEffectToTargetActor(TargetActor, DurationGameplayEffectClass);
+	}
 }
 
 void AAuraEffectActor::OnEndOverlap(AActor* TargetActor)
 {
+	if (InstantApplicationPolicy == EEffectApplicationPolicy::ApplyOnEndOverlap)
+	{
+		ApplyEffectToTargetActor(TargetActor, InstantGameplayEffectClass);
+	}
 	
+	if (DurationApplicationPolicy == EEffectApplicationPolicy::ApplyOnEndOverlap)
+	{
+		ApplyEffectToTargetActor(TargetActor, DurationGameplayEffectClass);
+	}
 }
