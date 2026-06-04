dc165ee69b74c72f65bc92d34f114ae999645367
AdvaitKhot003 <79973168+AdvaitKhot003@users.noreply.github.com>
2026-06-05 01:22:01 +0530

    Apply Gameplay Tags with Effects

--- Source/Aura/Private/AuraGameplayTags.cpp
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -5,5 +5,45 @@
 namespace AuraGameplayTags
 {
 	/** Input Tags **/
-	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Input_Move, "Input.Move", "Input tag for character movement input");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Input_Move, "Input.Move",
+		"Input tag for character's movement");
+	
+	/** Vital Attribute Tags **/
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Attribute_Vital_Health, "Attribute.Vital.Health",
+		"Attribute tag for character's health");
+	
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Attribute_Vital_Mana, "Attribute.Vital.Mana",
+		"Attribute tag for character's mana");
+	
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Attribute_Vital_MaxHealth, "Attribute.Vital.MaxHealth",
+		"Attribute tag for character's maximum health");
+	
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Attribute_Vital_MaxMana, "Attribute.Vital.MaxMana",
+		"Attribute tag for character's maximum mana");
+	
+	/** Primary Attribute Tags **/
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Attribute_Primary_Strength, "Attribute.Primary.Strength",
+		"Attribute tag for character's strength");
+	
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Attribute_Primary_Intelligence, "Attribute.Primary.Intelligence",
+		"Attribute tag for character's intelligence");
+	
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Attribute_Primary_Resilience, "Attribute.Primary.Resilience",
+		"Attribute tag for character's resilience");
+	
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Attribute_Primary_Vigor, "Attribute.Primary.Vigor",
+		"Attribute tag for character's vigor");
+	
+	/** Message Tags **/
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Message_Potion_Health, "Message.Potion.Health",
+		"Message tag for health potion to restore character's health");
+	
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Message_Potion_Mana, "Message.Potion.Mana",
+		"Message tag for mana potion to restore character's mana");
+	
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Message_Crystal_Health, "Message.Crystal.Health",
+		"Message tag for health crystal to restore character's health");
+	
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Message_Crystal_Mana, "Message.Crystal.Mana",
+		"Message tag for mana crystal to restore character's mana");
 }
--- Source/Aura/Public/AuraGameplayTags.h
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -8,4 +8,22 @@ namespace AuraGameplayTags
 {
 	/** Input Tags **/
 	AURA_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Input_Move);
+	
+	/** Vital Attribute Tags **/
+	AURA_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Attribute_Vital_Health);
+	AURA_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Attribute_Vital_Mana);
+	AURA_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Attribute_Vital_MaxHealth);
+	AURA_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Attribute_Vital_MaxMana);
+	
+	/** Primary Attribute Tags **/
+	AURA_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Attribute_Primary_Strength);
+	AURA_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Attribute_Primary_Intelligence);
+	AURA_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Attribute_Primary_Resilience);
+	AURA_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Attribute_Primary_Vigor);
+	
+	/** Message Tags **/
+	AURA_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Message_Potion_Health);
+	AURA_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Message_Potion_Mana);
+	AURA_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Message_Crystal_Health);
+	AURA_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Message_Crystal_Mana);
 }
